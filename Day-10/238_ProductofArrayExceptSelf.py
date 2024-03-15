03d42e27bcd131fc3c81fa7767af2286e88821a8
Shahid Saiyed <160094022+ShahidSaiyed2004@users.noreply.github.com>
2024-03-15 22:38:10 +0530

    Create 238_ProductofArrayExceptSelf.py

---
+++ Day-10/238_ProductofArrayExceptSelf.py
@@ -0,0 +1,18 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        p = [0] * n
+        s = [0] * n
+        p[0] = 1
+        s[n-1] = 1
+        ans = [0] * n
+
+        for i in range(1, n):
+            p[i] = p[i-1] * nums[i-1]
+        for i in range(n-2, -1, -1):
+            s[i] = s[i+1] * nums[i+1]
+        
+        for i in range(n):
+            ans[i] = p[i] * s[i]
+        
+        return ans
